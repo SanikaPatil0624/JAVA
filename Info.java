859ea07c89b35048a1364d377dec2072a86810c4
SanikaPatil5 <159874181+SanikaPatil0624@users.noreply.github.com>
2024-07-10 22:28:10 -0700

    Create Info.java

---
+++ Info.java
@@ -0,0 +1,17 @@
+class Info
+{
+	public static void main(String args[])
+	{
+		System.out.println("Name : Sanika Patil");
+		System.out.println("Adress : Chandur");
+		System.out.println("Age : 21");
+		System.out.println("Mob. No : xxxxxxx300");
+		System.out.println("Mail : san9ikapatil0624@gmail.com");
+		System.out.println("Class :SY_B.tech");
+		System.out.println("Course : CSE_AI");
+		System.out.println("CGPA: 8.80");
+
+
+	}
+
+}
