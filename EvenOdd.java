001dd410cbd77740d4c3cc83a15c4fc8caf7d47d
SanikaPatil5 <159874181+SanikaPatil0624@users.noreply.github.com>
2024-07-10 22:28:45 -0700

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,21 @@
+class EvenOdd
+{
+	public static void main(String args[])
+	{
+		int arr[]={1,2,3,4,5,6,7,8,9};
+		int i;
+		for(i=0;i<8;i++)
+		{
+			if(arr[i]%2==0)
+			{
+				System.out.println("Even");
+				System.out.println(arr[i]);
+			}
+			else
+			{										System.out.println("Odd");
+
+			}
+		}
+	}
+
+}
