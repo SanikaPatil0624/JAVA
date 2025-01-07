b733a9aab8749f769d2fd93c6fa8dc2eaeebbae2
SanikaPatil5 <159874181+SanikaPatil0624@users.noreply.github.com>
2025-01-07 20:40:50 +0530

    Update and rename Solution.java to Solution1.java

--- Solution1.java
+++ Solution1.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-class Solution {
+class Solution1 {
     public static void main(String[] args) {
         // Example usage
         String[] words1 = {"mass", "as", "hero", "superhero"};
