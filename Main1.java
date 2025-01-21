45ed91d207923065f425c0518a92f7b2dfef7efa
SanikaPatil5 <159874181+SanikaPatil0624@users.noreply.github.com>
2025-01-21 16:52:52 +0530

    Create Main1.java
    Write a program to calculate area and perimeter of Square. Side should be initialized with
    fixed value.

---
+++ Main1.java
@@ -0,0 +1,12 @@
+public class Main1{
+    private int side=20;
+    int area=side*side;
+    int perimeter=4*side;
+
+    public static void main(String a[])
+    {
+        Main ob=new Main();
+       System.out.println("Area of Square :"+ob.area);
+       System.out.println("Perimeter of Square :"+ob.perimeter);
+    }
+}
