1a660313c3133397d9d977930cfe2c8355bd4457
SanikaPatil5 <159874181+SanikaPatil0624@users.noreply.github.com>
2025-01-21 16:53:47 +0530

    Create Main2.java
    Write a program to perform following operations on double values.
    * Maximum from two double values (input from user).

---
+++ Main2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main2{
+    Scanner sc=new Scanner(System.in);
+    Double a=sc.nextDouble();
+    Double b=sc.nextDouble();
+    Double res=Math.max(a,b);
+    //Double res=Double.max(a,b);
+     
+    public static void main(String a[])
+    {
+        Main ob=new Main();
+       System.out.println("Greater Value :"+ob.res);
+      
+    }
+}
+./.
