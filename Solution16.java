5e781d2cb3ac4276aa6c831388a5bdc86cafaffb
SanikaPatil5 <159874181+SanikaPatil0624@users.noreply.github.com>
2025-01-27 21:07:12 +0530

    Create Solution16.java
    Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
    
    An input string is valid if:
    
    Open brackets must be closed by the same type of brackets.
    Open brackets must be closed in the correct order.
    Every close bracket has a corresponding open bracket of the same type.
     
    
    Example 1:
    
    Input: s = "()"
    
    Output: true
    
    Example 2:
    
    Input: s = "()[]{}"
    
    Output: true
    
    Example 3:
    
    Input: s = "(]"
    
    Output: false
    
    Example 4:
    
    Input: s = "([])"
    
    Output: true

---
+++ Solution16.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Solution16 {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+           
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } 
+            
+            else {
+              
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+    
+    public static void main(String[] args) {
+        Solution16 ob = new Solution16();
+        String ex = "[]{";
+        boolean res = ob.isValid(ex);
+        System.out.println(res); // Output: true
+    }
+}
