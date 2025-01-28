f701d853a12cac2335b27ac9c1c5ec041b10e4d1
SanikaPatil5 <159874181+SanikaPatil0624@users.noreply.github.com>
2025-01-28 20:32:25 +0530

    Create str.java

---
+++ str.java
@@ -0,0 +1,13 @@
+// Java Program to demonstrate String
+public class str {
+
+    // Main Function
+    public static void main(String args[])
+    {
+
+        // creating Java string using new keyword
+        String str = new String("GeeksforGeeks");
+
+        System.out.println(str);
+    }
+}
