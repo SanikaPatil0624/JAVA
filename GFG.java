0563cf42a5455c8ad96a12348409505c4424646b
SanikaPatil5 <159874181+SanikaPatil0624@users.noreply.github.com>
2025-02-02 21:39:00 +0530

    Create GFG.java

---
+++ GFG.java
@@ -0,0 +1,22 @@
+// Java Program to Add Elements to a List
+import java.util.*;
+
+class GFG {
+  
+    public static void main(String args[])
+    {
+        // Creating an object of List interface,
+        // implemented by ArrayList class
+        List<String> al = new ArrayList<>();
+
+        // Adding elements to object of List interface
+        // Custom elements
+        al.add("Geeks");
+        al.add("Geeks");
+        al.add(1, "For");
+
+        // Print all the elements inside the
+        // List interface object
+        System.out.println(al);
+    }
+}
